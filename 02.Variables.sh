8afc538d9b90be424d1ed9bd0a2bd268807d3469
myusername <myemail>
2024-07-25 23:27:30 +0530

    commit the shell script

--- 02.Variables.sh
+++ 02.Variables.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-Person1=Basha>s
-Person2=Chesmin.B
+Person1=Basha
+Person2=Chesmin
 
 echo "$Person1: Hello $Person2, Good Morning"
 echo "$Person2: Hello $Person1,Very Good Morning"
-echo "$Ramesh: How are you $Person2?"
+echo "$Person1: How are you $Person2?"
 echo "$Person2: i am good $Person1,How are you"
