b22d887528281d9fe6d7dee88aed3e18fcce7a23
myusername <myemail>
2024-07-25 23:43:47 +0530

    commit the shell script

--- 04.Variables.sh
+++ 04.Variables.sh
@@ -3,6 +3,13 @@
 PERSON1=$1
 PERSON2=$2
 
+# we have to give names in command ex:- sh 04.Variables.sh basha chesu
+# output:- basha: Hello chesu, Good Morning
+#          chesu: Hi basha, very Good Morning
+#          basha: How are you chesu?
+#          chesu: I am good basha, How are you
+
+
 echo "$PERSON1: Hello $PERSON2, Good Morning"
 echo "$PERSON2: Hi $PERSON1, very Good Morning"
 echo "$PERSON1: How are you $PERSON2?"
