5f11be8f83eeebbbe71cea159cdbf7891768fb2a
myusername <myemail>
2024-07-25 23:00:37 +0530

    commit the shell script

--- 02.Variables.sh
+++ 02.Variables.sh
@@ -3,5 +3,5 @@
 
 echo "Ramesh: Hello Suresh, Good Morning"
 echo "Suresh: Hello Ramesh,Very Good Morning"
-echo "Ramesh: How are Suresh?"
-echo "Suresh: i am good Ramesh"
+echo "Ramesh: How are you Suresh?"
+echo "Suresh: i am good Ramesh,How are you"
