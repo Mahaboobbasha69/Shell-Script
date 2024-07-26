8868e2a9793a1874234e0c9f9c40a0a1a30ba725
myusername <myemail>
2024-07-26 11:52:04 +0530

    commit the shell script

--- 05.Variables.sh
+++ 05.Variables.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "Please enter username: "
+echo "Please enter username:: "
 
 read -s USERNAME #the value entered above will be automatically atteched to USERNAME variable
 
-echo "Please enter Password: "
+echo "Please enter Password:: "
 
 read -s PASSWORD 
 
