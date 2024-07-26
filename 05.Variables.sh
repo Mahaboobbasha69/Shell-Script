3e24da6f1b6b00b051651cd4125df6d9bab4a6b6
myusername <myemail>
2024-07-26 11:44:27 +0530

    commit the shell script

---
+++ 05.Variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Please enter username: "
+
+read -s USERNAME #the value entered above will be automatically atteched to USERNAME variable
+
+echo "Please enter Password: "
+
+read -s PASSWORD 
+
+echo "username is : $USERNAME , Password is : $PASSWORD" # I am priting just for validation, you should not print username and passwords in scripts
+
