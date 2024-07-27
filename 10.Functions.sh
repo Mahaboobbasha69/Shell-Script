32fc39d4a38dafa58097b7f04b8ec6c40ddb8d24
myusername <myemail>
2024-07-27 21:39:40 +0530

    commit the shell script

---
+++ 10.Functions.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+if [ $ID -ne 0 ]
+then
+    echo "You are not a root user 'please run into root user'"
+    exit 1
+else 
+    echo "you are root user"
+fi
+
+Method_Calling (){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2.... Failed"
+        exit 1
+    else
+        echo "$2 Success"
+    fi
+}
+
+yum install mysql -y
+
+Method_Calling $? "Mysql installed"
+
+yum install git -y
+
+Method_Calling $? "git installed"
