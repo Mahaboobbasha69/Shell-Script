c7025cafbef8c333aff9b142b970bb65ed6b5189
myusername <myemail>
2024-07-28 00:57:32 +0530

    commit the shell script

---
+++ 13.install-packages.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+
+if [$ID -ne 0]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
+
+Method_Calling (){
+
+    if [ $1 -ne 0 ]
+    then
+        echo "Error:: $2.....Failed"
+        exit 1
+    else
+        echo "$2.....Success"
+    fi
+}
+
+for $Package in $@
+do
+  yum installed $Package
+  if [ $? -ne 0 ]
+  then
+      yum install $Package -y
+      Method_Calling $? "Installing of $Package"
+  else
+      echo "Already installed $Package"
+  fi
+      #
+
