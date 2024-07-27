a779109c109efd4fedb11bce3a3fc6f93e20d7aa
myusername <myemail>
2024-07-28 01:21:36 +0530

    commit the shell script

--- 13.install-package.sh
+++ 13.install-package.sh
@@ -2,10 +2,20 @@
 
 ID=$(id -u)
 
+Red="\e[31m"
+Green="\e[32m"
+Yellow="\e[33m"
+Normal="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
+
 
 if [$ID -ne 0]
 then
-    echo -e "$R ERROR:: Please run this script with root access $N"
+    echo -e "$Red ERROR:: Please run this script with root access $Normal"
     exit 1 # you can give other than 0
 else
     echo "You are root user"
@@ -15,21 +25,21 @@ Method_Calling (){
 
     if [ $1 -ne 0 ]
     then
-        echo "Error:: $2.....Failed"
+        echo "$Red Error:: $2.....Failed"
         exit 1
     else
-        echo "$2.....Success"
+        echo "$Green $2.....Success"
     fi
 }
 
 for Package in $@
 do
-  yum installed $Package
+  yum installed $Package &>> $LOGFILE
   if [ $? -ne 0 ]
   then
-      yum install $Package -y
+      yum install $Package -y &>> $LOGFILE
       Method_Calling $? "Installing of $Package"
   else
-      echo "Already installed $Package"
+      echo "$Green Already installed $Package"
   fi
 done 
